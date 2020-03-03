9c6bdfe91f8e808d6f1a4cc640587e1e663e3c9f
Joe SHI <qiaoshi@amazon.com>
2020-03-03 20:15:38 +0800

    delete unit test in buildspec.yml

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -53,14 +53,30 @@ for f in *.yaml; do
 done
 
 cd ..
-echo "Updating code source bucket in template with $1"
-replace="s/%%BUCKET_NAME%%/$1/g"
-echo "sed -i '' -e $replace $template_dist_dir/*.template"
-sed -i '' -e $replace $template_dist_dir/*.template
-replace="s/%%SOLUTION_NAME%%/$2/g"
-echo "sed -i '' -e $replace $template_dist_dir/*.template"
-sed -i '' -e $replace $template_dist_dir/*.template
-replace="s/%%VERSION%%/$3/g"
-echo "sed -i '' -e $replace $template_dist_dir/*.template"
-sed -i '' -e $replace $template_dist_dir/*.template
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Updating code source bucket in template with $1"
+    replace="s/%%BUCKET_NAME%%/$1/g"
+    echo "sed -i '' -e $replace $template_dist_dir/*.template"
+    sed -i '' -e $replace $template_dist_dir/*.template
+    replace="s/%%SOLUTION_NAME%%/$2/g"
+    echo "sed -i '' -e $replace $template_dist_dir/*.template"
+    sed -i '' -e $replace $template_dist_dir/*.template
+    replace="s/%%VERSION%%/$3/g"
+    echo "sed -i '' -e $replace $template_dist_dir/*.template"
+    sed -i '' -e $replace $template_dist_dir/*.template
+else
+    echo "Updating code source bucket in template with $1"
+    replace="s/%%BUCKET_NAME%%/$1/g"
+    echo "sed -i -e $replace $template_dist_dir/*.template"
+    sed -i -e $replace $template_dist_dir/*.template
+    replace="s/%%SOLUTION_NAME%%/$2/g"
+    echo "sed -i -e $replace $template_dist_dir/*.template"
+    sed -i -e $replace $template_dist_dir/*.template
+    replace="s/%%VERSION%%/$3/g"
+    echo "sed -i -e $replace $template_dist_dir/*.template"
+    sed -i -e $replace $template_dist_dir/*.template
+fi
+
+
 
